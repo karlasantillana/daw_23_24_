daf609de377c2199f730a399c7f91317dd25768c
karlasantillana <karlasantillanaayulo@gmail.com>
2023-10-05 12:43:11 +0200

    ejerciios iniciales

--- UT2. Ejercicios_iniciales/ej21_agenda.php
+++ UT2. Ejercicios_iniciales/ej21_agenda.php
@@ -18,6 +18,13 @@ válidos.
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda</title>
+
+    <style>
+        table,td{
+            border: 1px solid;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -46,21 +53,23 @@ válidos.
 
         echo "<table>
                     <tr>
-                        <th>agenda</th>
+                        <th colspan='3'>agenda</th>
                     </tr>
 
                     <tr>
                         <td>clave</td>
                         <td>clave</td>
                         <td>contenido</td>
-                    </tr>"
-            foreach($agenda as $a => $b){
+                    </tr>";
+            foreach($agenda as $contacto => $datos){
                 echo "<tr>
-                        <td>$a</td>";
-                foeach($agenda[$a])
-                </tr>"
-            }.
-            
+                        <td rowspan='4'>$contacto</td>";
+                foreach($agenda[$contacto] as $clave=> $contenido){
+                    echo "<td>$clave</td>
+                        <td>$contenido</td>
+                    </tr>";
+                }
+            }
             "</table>";
 
 
